71518f5cf26e473b39fb22165f04a8f25232a19b
stg <a.sergiotorres@gmail.com>
2024-02-10 23:13:12 +0100

    bucles > challenge 1

---
+++ bucles.js
@@ -0,0 +1,35 @@
+// **Bucles**
+// 1. Escribir un script que pida un valor entero y muestre en pantalla una lista de números desde el 0 al valor tecleado. Los números se separarán por comas.
+
+let num = 5
+let lista = []
+for (let i = 0; i<num; i++) {
+    lista.push(i)
+    
+}
+console.log(lista.join(","))
+
+// 2. Hacer un programa que cuente de N en N hasta un número M proporcionado por el usuario. El usuario también ha de proporcionar N.
+
+// 3. Genera una lista con todos los números pares positivos por debajo del número tecleado por el usuario.
+
+// 4. Escribe la tabla de multiplicar de un número introducido por el usuario.
+
+// 5. Formar un triángulo de este estilo
+// *
+// **
+// ***
+// ****
+
+// 6. Con la cantidad de líneas que indique el usuario.
+
+// 7. Calcula la suma de todos los números enteros positivos entre 0 y el número tecleado por el usuario.
+
+// 8. Pide al usuario que teclee 4 números (uno cada vez). Con esos números el programa deberá calcular la media aritmética (la suma de todos ellos dividido por la cantidad de números).
+
+// 8. Saca por consola los números primos del 0 al número que introduzca un usuario por prompt.
+
+// 9. Dado un número entero, muestra una cuenta atrás desde el número tecleado hasta el 0.
+// Modifica el programa para que el usuario introduzca dos números y se efectúe una cuenta atrás desde el primer número hasta el segundo. Modifícalo de nuevo para que el programa determine qué número es el mayor y cuente desde el mayor hasta el menor.
+
+// 10. Pide al usuario cinco números, almacénalos en un array y muestra por consola el resultado de cada uno de ellos multiplicado por 3. Muestra también un array que contenga todos los resultados.
