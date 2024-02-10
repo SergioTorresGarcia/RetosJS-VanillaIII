2c9177255039655e88e06a3c835fc9c3b0faf98c
stg <a.sergiotorres@gmail.com>
2024-02-10 22:57:50 +0100

    challenge 11

--- main.js
+++ main.js
@@ -90,18 +90,39 @@
 
 // 10. Crea un programa en el que el usuario introduzca el número del mes (del 1 al 12) y el código responda cuántos días tiene ese mes.
 
-let numMes = 5
+// let numMes = 5
 
-switch (numMes) {
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        console.log(`El mes ${numMes} tiene 30 dias`)
-        break;
-    case 2:
-        console.log("Febrero tiene 28 dias (o 29, cada 4 años)")
-        break;
-    default:
-        console.log(`El mes ${numMes} tiene 31 dias`)
+// switch (numMes) {
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         console.log(`El mes ${numMes} tiene 30 dias`)
+//         break;
+//     case 2:
+//         console.log("Febrero tiene 28 dias (o 29, cada 4 años)")
+//         break;
+//     default:
+//         console.log(`El mes ${numMes} tiene 31 dias`)
+// }
+
+// 11. Crea un script que pida al usuario el diámetro de una rueda y su grosor (en metros) y realice las siguientes operaciones:
+
+//     a) Si el diámetro es superior a 1.4 metros debe mostrarse el mensaje “La rueda es para un vehículo grande”. Si es menor o igual a 1.4 metros pero mayor que 0.8 debe mostrarse el mensaje “La rueda es para un vehículo mediano”. Si no se cumplen ninguna de las condiciones anteriores debe mostrarse por pantalla el mensaje “La rueda es para un vehículo pequeño”.
+
+//     b) Si el diámetro es superior a 1.4 con un grosor inferior a 0.4, o si el diámetro es menor o igual a 1.4 pero mayor que 0.8, con un grosor inferior a 0.25, deberá mostrarse el mensaje “El grosor para esta rueda es inferior al recomendado”
+
+let diametro = 1.90
+let grosor = 0.40
+//a)
+if (diametro > 1.4) {
+    console.log("La rueda es para un vehículo grande")
+} else if (0.8 < diametro && diametro <= 1.4) {
+    console.log("La rueda es para un vehículo mediano")
+} else {
+    console.log("La rueda es para un vehículo pequeño")
+}
+//b)
+if ((diametro > 1.4 && grosor < 0.4) || (diametro <= 1.4 && diametro > 0.8 && grosor < 0.25)) {
+    console.log("El grosor para esta rueda es inferior al recomendado")
 }
