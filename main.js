f12f16c4e75a69bbedddc44b0615c091b4a5be3f
stg <a.sergiotorres@gmail.com>
2024-02-10 12:35:50 +0100

    challenge 2

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 
 // 1. Pedir dos números y decir si son múltiplos o no.
-let num1 = 60
-let num2 = 15
+// let num1 = 60
+// let num2 = 15
 
-if (num1 % num2 == 0) { 
-    console.log(`${num1} es múltiplo de ${num2}`)
-} else if (num2 % num1 == 0) { 
-    console.log(`${num2} es múltiplo de ${num1}`)
+// if (num1 % num2 == 0) { 
+//     console.log(`${num1} es múltiplo de ${num2}`)
+// } else if (num2 % num1 == 0) { 
+//     console.log(`${num2} es múltiplo de ${num1}`)
+// } else {
+//     console.log("No son múltiplos")
+// }
+
+// 2. Pedir dos números y decir cual es el mayor.
+let num1 = 150
+let num2 = 150
+
+if (num1 < num2) { 
+    console.log(`${num1} es menor que ${num2}`)
+} else if (num1 > num2) { 
+    console.log(`${num1} es mayor que ${num2}`)
 } else {
-    console.log("No son múltiplos")
+    console.log(`${num1} es igual a ${num2}`)
 }
