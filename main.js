8f2fc6163e93798bd4e2107062a51a27e750835b
stg <a.sergiotorres@gmail.com>
2024-02-10 12:39:53 +0100

    challenge 3

--- main.js
+++ main.js
@@ -12,13 +12,24 @@
 // }
 
 // 2. Pedir dos números y decir cual es el mayor.
-let num1 = 150
-let num2 = 150
+// let num1 = 150
+// let num2 = 150
 
-if (num1 < num2) { 
-    console.log(`${num1} es menor que ${num2}`)
-} else if (num1 > num2) { 
-    console.log(`${num1} es mayor que ${num2}`)
+// if (num1 < num2) { 
+//     console.log(`${num1} es menor que ${num2}`)
+// } else if (num1 > num2) { 
+//     console.log(`${num1} es mayor que ${num2}`)
+// } else {
+//     console.log(`${num1} es igual a ${num2}`)
+// }
+
+// 3. Pedir un número y decir si es un número negativo, si es positivo o cero.
+let num = parseInt(0)
+
+if (num > 0) {
+    console.log(`${num} es un número positivo`)
+} else if (num < 0) {
+    console.log(`${num} es un número negativo`)
 } else {
-    console.log(`${num1} es igual a ${num2}`)
+    console.log(`${num} es cero`)
 }
